15e04b5854774c65196f4140082babac2f9209b6
Franco Sanchez Alejandro <francosanche@gmail.com>
2022-09-19 17:29:53 -0300

    no entiendo porque no funciona

--- custom_JS/test.js
+++ custom_JS/test.js
@@ -1,11 +1,46 @@
 /* Este codigo es para hacer pruebas nada mas, no ira en el proyecto final*/
 
 // const content_container_1 = document.querySelector(".content_1")
+function create_element_basic_node(tag,clases){
+    return document.createElement(`${tag}`).classList = clases
+}
 
-let pipi = document.createElement("div")
-console.log(pipi)
-console.log("hola")
-pipi.innerHTML = "<h2 class=\"text-center mt-1\">Ventaja</h2><p class=\"text-center\">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non enim ipsam a porroquae! Nam expeditavoluptatum laborum officia, dolor voluptatem explicabo. Ut suscipit, minus dolores expedita enimsimilique dolorum!</p>"
-console.log(pipi)
+function create_element_basic_clases(tag){
+    return create_element_basic_node(tag,['text-center', 'mt-1'])
+}
+
+let name = "jorge"
+let title = "casfsa"
+let expirience = 3
+let disponible = true
+
+const container = document.querySelector('.content_1')
+const div = document.createElement('div')
+div.classList = ['col', 'mr-4', 'bg-info', 'border', 'border-2', 'border-success', 'rounded-3', 'shadow_custom_black']
+
+const h2_name = create_element_basic_clases('h2')
+h2_name.textContent = name
+
+
+const h3_title = create_element_basic_clases('h3')
+h3_title.textContent = title
+
+const h3_expirience = create_element_basic_clases('h3')
+h3_expirience.textContent = expirience
+
+
+const h3_disponible = create_element_basic_clases('h3')
+h3_disponible.textContent = disponible
+
+div.append(h2_name)
+div.append(h3_title)
+div.append(h3_expirience)
+div.append(h3_disponible)
+
+let container_content = document.querySelector(".content_1")
+container_content.append(div)
+
+
+
 
 
